d148cb449a5c8d8245298fc8d13d7463aa03b44f
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-07-13 23:27:38 -0400

    update

--- app01/app01/src/App.js
+++ app01/app01/src/App.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import Header from './componentes/header'
-import Corpo from './componentes/corpo'
-import './App.css'
-
+import React, {useState} from 'react'
 
 export default function App(){
-  const textoDestaque={
-    color:'#00f',
-    fontSize: '3em'
+
+  const [form, setform]=useState({'nome':'', 'idade':'', 'sexo':''})
+
+  const handleChangeName = (e) =>{
+    if(e.target.getAttribute('name') == 'fname') {
+      setform({'name':e.target.value, 'idade': form.idade, 'sexo': form.sexo})
+    } else if(e.target.getAttribute('name') == 'fidade') {
+      setform({'name': form.name, 'idade': e.target.value, 'sexo': form.sexo})
+    } else if(e.target.getAttribute('name') == 'fsexo') {
+      setform({'name': form.name, 'idade': form.idade, 'sexo': e.target.value})
+    }
   }
 
   return(
     <>
-    <section className='caixa'>
-      <h1 style={{color: '#f00', fontSize: '5em'}}>Curso de React</h1>
-      <h2 style={textoDestaque}>Primeiro estilização em React, usando CSS</h2>
-      <p className='texto'>Como fazer uma estilização de CSS em react, pode ser feito, inline, interno ou externo. Porém, sempre dando preferencia para o uso de classes</p>
-      <p className='texto'>No uso do css, utiliza-se Camel Case e a separação deve ser feita por meio de uma virgula, e nao um ponto e virgula.</p>
-    </section>
+      <label>Nome: </label>
+      <input type='text' name='fname' value={form.nome} onChange={(e)=>handleChangeName(e)}/>
+      <label>Idade: </label>
+      <input type='text' name='fidade' value={form.idade} onChange={(e)=>handleChangeName(e)}/>
+      <label>Sexo: </label>
+      <input type='text' name='fsexo' value={form.sexo} onChange={(e)=>handleChangeName(e)}/>
+
+      <p>Nome digitando: {form.name}</p>
+      <p>Idade digitando: {form.idade}</p>
+      <p>Sexo digitando: {form.sexo}</p>
     </>
   )
 }
