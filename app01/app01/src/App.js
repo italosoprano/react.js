b5963f1031b64b28d4f4efa600e5c1a4a2c1c25f
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-07-20 21:47:39 -0400

    update

--- app01/app01/src/App.js
+++ app01/app01/src/App.js
@@ -39,14 +39,68 @@ const tabelaImc = () =>{
   )
 }
 
+const f_peso = (p, sp) =>{
+  return (
+    <>
+      <label>Insira seu peso: </label>
+      <input type='text' value={p} onChange={(e)=>{sp(e.target.value)}}/>
+    </>
+  )
+}
+
+const f_altura = (a, sa) =>{
+  return (
+    <>
+      <label>Insira sua altura: </label>
+      <input type='text' value={a} onChange={(e)=>{sa(e.target.value)}}/>
+    </>
+  )
+}
+
+const fcalcular = (p, a, sr) =>{
+  const calc = () =>{
+    sr (p/(a*a))
+  }
+  
+  return (
+    <button onClick={calc()}>Calcular</button>
+  )
+}
+
+const f_resultado = (r) =>{
+  return (
+    <>
+      <div>
+        Resultado: {r.toFixed(2)}
+      </div>
+    </>
+  )
+}
+
 export default function App(){
 
+  const [peso, setPeso]=useState(0)
+  const [altura, setAltura]=useState(0)
+  const [resultado, setResultado]=useState(0)
 
   return(
     <>
       <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh'}}>
+        {f_peso(peso, setPeso)}
+        {f_altura(altura, setAltura)}
+        {fcalcular(peso, altura, setResultado)}
+        {f_resultado(resultado)}
         {tabelaImc()}
       </div>
     </>
   )
 }
+
+/*
+  
+      style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh'}}
+      {f_resultado(resultado)}
+  <div >
+        {tabelaImc()}
+      </div>
+*/
